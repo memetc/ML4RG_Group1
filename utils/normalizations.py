51a5752bc1b14d14141b2b67aa779cf7c3ce234c
memetc <mehmetcelimli820@gmail.com>
2024-07-05 02:13:16 +0200

    normalization func docstrings

--- utils/normalizations.py
+++ utils/normalizations.py
@@ -2,6 +2,21 @@ import pandas as pd
 import numpy as np
 
 def calculate_mean(row, stress_cols, control_cols):
+    """
+    Calculate the mean of the ratios of stress values to control values.
+
+    This function computes the mean of the ratios between stress columns and control columns for a given row.
+    It only includes ratios where both stress and control values are non-NA and the control value is not zero.
+
+    Parameters:
+    row (pd.Series): A row from a DataFrame.
+    stress_cols (list of str): List of column names representing stress conditions.
+    control_cols (list of str): List of column names representing control conditions.
+
+    Returns:
+    float: The mean of the ratios of stress values to control values for the given row.
+           Returns 0 if no valid ratios are found.
+    """
     values = []
     for stress_col, control_col in zip(stress_cols, control_cols):
         stress_val = row[stress_col]
@@ -25,6 +40,17 @@ def get_ctrl_norm(data_df, stress_columns, control_columns):
     )
 
 def get_mean(data_df, stress_columns):
+    """
+    Normalize data by control columns.
+
+    Parameters:
+    data_df (pd.DataFrame): DataFrame containing the data to be normalized.
+    stress_columns (list of str): List of column names representing stress conditions.
+    control_columns (list of str): List of column names representing control conditions.
+
+    Returns:
+    pd.Series: A series containing the normalized values for each row in the data frame.
+    """
     np.mean(
         [
             data_df[stress_columns[0]],
