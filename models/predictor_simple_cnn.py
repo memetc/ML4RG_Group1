8116fa2edb69d403594c86844bca20ccbda014f1
memetc <mehmetcelimli820@gmail.com>
2024-07-16 13:15:36 +0200

    stride is added

--- models/predictor_simple_cnn.py
+++ models/predictor_simple_cnn.py
@@ -2,13 +2,13 @@ import torch.nn as nn
 
 
 class SimpleCNN(nn.Module):
-    def __init__(self, num_features, cnn_filters, hidden_size, seq_length, kernel_size=3):
+    def __init__(self, num_features, cnn_filters, hidden_size, seq_length, kernel_size=3, stride=1):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv1d(num_features, cnn_filters, kernel_size=kernel_size)
+        self.conv1 = nn.Conv1d(num_features, cnn_filters, kernel_size=kernel_size, stride=stride)
         self.relu = nn.ReLU()
 
         # Calculate the output sequence length after conv1 layer
-        conv1_output_length = seq_length - kernel_size + 1
+        conv1_output_length = (seq_length - kernel_size) // stride + 1
         self.conv1_output_size = cnn_filters * conv1_output_length
 
         self.fc1 = nn.Linear(self.conv1_output_size, hidden_size)
